3c2858a89cebe37e440c0ad525e33607c6f0b223
Yogaraj.S <78194098+yogarajjuju@users.noreply.github.com>
2025-03-27 15:55:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (79.44%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.java
@@ -1,25 +1,47 @@
 class Solution {
     public boolean checkIfExist(int[] arr) {
-        Arrays.sort(arr);
-        for(int i =0;i<arr.length;i++){
-            if(binarysearch(arr,2*arr[i],i)){
+        for(int i = 0; i<arr.length;i++){
+            int num = 2*arr[i];
+            if(check(arr,num,i)){
                 return true;
-            } 
-        }
-        return false;
-    }
-    public boolean binarysearch(int []arr , int target,int exclude){
-        int start = 0, end = arr.length-1;
-        while(start<=end){
-            int mid = start+(end-start)/2;
-            if(arr[mid]==target&& mid!=exclude){
-                return true;
-            } else if(arr[mid]<target){
-                start=mid+1;
-            } else{
-                end = mid-1;
             }
         }
-        return false;
-    }
+         return false;
+           
+        }
+        public boolean check(int arr[], int num , int index){
+            for(int i = 0;i<arr.length;i++){
+                if(index!=i&&arr[i]==num){
+                    return true;
+                }
+            }
+            return false;
+        }
 }
+
+
+
+
+// using binarysearch
+ //     Arrays.sort(arr);
+    //     for(int i =0;i<arr.length;i++){
+    //         if(binarysearch(arr,2*arr[i],i)){
+    //             return true;
+    //         } 
+    //     }
+    //     return false;
+    // }
+    // public boolean binarysearch(int []arr , int target,int exclude){
+    //     int start = 0, end = arr.length-1;
+    //     while(start<=end){
+    //         int mid = start+(end-start)/2;
+    //         if(arr[mid]==target&& mid!=exclude){
+    //             return true;
+    //         } else if(arr[mid]<target){
+    //             start=mid+1;
+    //         } else{
+    //             end = mid-1;
+    //         }
+    //     }
+    //     return false;
+    // }
