d438596c070bf48548558dd780ffc8e533ccf487
Yogaraj.S <78194098+yogarajjuju@users.noreply.github.com>
2025-04-08 15:01:49 +0530

    Time: 6 ms (14.87%) | Memory: 45.5 MB (21.38%) - LeetSync

--- 921-spiral-matrix-iii/spiral-matrix-iii.java
+++ 921-spiral-matrix-iii/spiral-matrix-iii.java
@@ -1,25 +1,35 @@
-class Solution {
+public class Solution {
     public int[][] spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
-        int [] [] dir = {{0,1},{1,0},{0,-1},{-1,0}};
-        List<int[]> ans= new ArrayList<>();
-        int r= rStart, c=cStart;
-        int diri=0;
-        int steps=1;
-        ans.add(new int[] {r,c});
-        while(ans.size()<rows*cols){
-            for(int j =0;j<2;j++){
-                for(int i =0;i<steps;i++){
-                    r+=dir[diri][0];
-                    c+=dir[diri][1];
-                    if(r>=0&&r<rows&& c>=0 &&c<cols){
-                        ans.add(new int[]{r,c});
+        // Directions: East, South, West, North
+        int[][] directions = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+        List <int[]> ans = new ArrayList<>();
+
+        // Initial coordinates and direction index
+        int r = rStart, c = cStart;
+        int directionIdx = 0;
+        int steps = 1; // Number of steps to take in current direction
+        
+        ans.add(new int[] {rStart, cStart}); // Add starting point
+
+        while (ans.size() < rows * cols) {
+            for (int i = 0; i < 2; i++) {
+                // Looping twice for same number of steps
+                for (int j = 0; j < steps; j++) {
+                    r += directions[directionIdx][0];
+                    c += directions[directionIdx][1];
+
+                    // Check if within bounds
+                    if (r >= 0 && r < rows && c >= 0 && c < cols) {
+                        ans.add(new int[]{r, c});
                     }
                 }
-                diri =(diri+1)%4;
-
+                // Change direction
+                directionIdx = (directionIdx + 1) % 4;
             }
-           steps++;
+            steps++; // Increase the steps after traversing in each direction pair
         }
-         return ans.toArray(new int[rows*cols][2]);
+
+        return ans.toArray(new int[rows * cols][2]);
     }
+
 }
