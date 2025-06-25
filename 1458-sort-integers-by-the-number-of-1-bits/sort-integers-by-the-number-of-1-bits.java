1bdc9a4fd9f47ff7861f37118729edcdd1b8f120
Yogaraj.S <78194098+yogarajjuju@users.noreply.github.com>
2025-06-25 22:51:12 +0530

    Time: 3 ms (99.39%) | Memory: 44.6 MB (92.35%) - LeetSync

--- 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
+++ 1458-sort-integers-by-the-number-of-1-bits/sort-integers-by-the-number-of-1-bits.java
@@ -2,12 +2,12 @@ class Solution {
     public int[] sortByBits(int[] arr) {
         int n = arr.length;
         for(int i =0;i<n;i++){
-            int bitcount = Integer.bitCount(arr[i]);
-            arr[i]+=bitcount*100000;
+            int bitcount = Integer.bitCount(arr[i]);// to get the bit 
+            arr[i]+=bitcount*100000;//prioritize the bit
         }
         Arrays.sort(arr);
         for(int i=0;i<n;i++){
-            arr[i]%=100000;
+            arr[i]%=100000;// to return the origina arr
         }
         return arr;
         
