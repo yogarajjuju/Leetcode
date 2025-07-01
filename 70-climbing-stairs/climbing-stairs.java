9bb72b17d8119c66059c03314f989478f6a09adc
Yogaraj.S <78194098+yogarajjuju@users.noreply.github.com>
2025-07-01 09:52:36 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (5.31%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        if(n==1) return 1;
+        if(n==2) return 2;
+       int f = 1, s = 2, t=3;
+       for(int i=3;i<=n;i++){
+        t = f+s;
+        f =s;
+        s=t;
+       }
+       return s;
+        
+    }
+}
