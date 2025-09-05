8e2cd6a4cb58bd2166175c529613455844fececf
Yogaraj.S <78194098+yogarajjuju@users.noreply.github.com>
2025-09-05 12:21:53 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (75.66%) - LeetSync

--- 202-happy-number/happy-number.java
+++ 202-happy-number/happy-number.java
@@ -3,11 +3,18 @@ class Solution {
     // by using fast and skow pointer method ;
     public boolean isHappy(int n) {
         int slow = n , fast = n;
+        //while loop is not used here because initially slow and 
+//fast pointer will be equal only, so the loop won't run.
         do{
+            //slow moving one step ahead and fast moving two steps ahead
             slow=findSquare(slow); // move slow pointer by 1 i.e  find sqrt 1 time 
             fast=findSquare(findSquare(fast));  // move the fast pointer 2 times i.e find sqrt 2x times than slow pointer
 
         } while(slow!=fast);
+
+        //if a cycle exists, then the number is not a happy number
+//and slow will have a value other than 1
+
         if(slow==1){
             return true;
         }
