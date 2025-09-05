b3cc16d841c379462ac960b3b2ea3374ea4cf839
Yogaraj.S <78194098+yogarajjuju@users.noreply.github.com>
2025-09-05 14:26:31 +0530

    Time: 0 ms (100.00%) | Memory: 44.2 MB (89.59%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.java
+++ 141-linked-list-cycle/linked-list-cycle.java
@@ -12,7 +12,7 @@
 public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode fast =head, slow = head;
-        // Floyd’s Cycle Detection or fast and slow pointer or totoise and hare method 
+        // Floyd’s Cycle Detection or fast and slow pointer or tortise and hare method 
 
         while(fast!=null&& fast.next!=null){
             slow=slow.next;
