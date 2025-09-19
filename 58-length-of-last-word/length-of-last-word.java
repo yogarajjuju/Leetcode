3611bf2b11f4bd21c8fc9e21bf00d8991800f6df
Yogaraj.S <78194098+yogarajjuju@users.noreply.github.com>
2025-09-19 11:22:15 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (82.15%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,13 +1,23 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int count =0;
-        for(int i=0;i<s.length();i++){
-            if(s.charAt(i)!=' '){//to check the sentence has space if no we can count the letters 
-                count++;
-            } else if(i+1<s.length()&& s.charAt(i+1)!=' '){// if we found the space we  reset the counter to 0
-                count=0;
+        // trim the spaces 
+        // declare a len var =0;
+        // itreate in reverse 
+        //ifs.charat(i)!=' ' len++
+        //elseif(length>0) break 
+
+        // then return the length
+
+        s=s.trim();
+        int len=0;
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)!=' '){
+                len++;
+            } else if(len>0){
+                break;
             }
         }
-        return count;
+        return len;
+        
     }
 }
